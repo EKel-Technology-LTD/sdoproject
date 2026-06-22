9f275a1f466ad605f44cf47ab3c42b9594ae14e9
Xiao Mei <no-reply@ekeltechnology.com>
2026-06-22 19:25:47 +0000

    fix: address Lead create review feedback

--- force-app/main/default/aura/SDO_InsideSalesLeadNew/SDO_InsideSalesLeadNewHelper.js
+++ force-app/main/default/aura/SDO_InsideSalesLeadNew/SDO_InsideSalesLeadNewHelper.js
@@ -1,33 +1,46 @@
+/* eslint-disable no-unused-expressions */
 ({
-    routeUser: function (component) {
-        var action = component.get('c.canUseInsideSalesLeadCreate');
-        action.setCallback(this, function (response) {
-            if (response.getState() === 'SUCCESS' && response.getReturnValue() === true) {
-                component.set('v.checkedAccess', true);
-                var flow = component.find('insideSalesLeadFlow');
-                flow.startFlow('SDO_Inside_Sales_Create_Lead');
-                return;
+  routeUser: function (component) {
+    var action = component.get("c.canUseInsideSalesLeadCreate");
+    action.setCallback(this, function (response) {
+      if (
+        response.getState() === "SUCCESS" &&
+        response.getReturnValue() === true
+      ) {
+        component.set("v.checkedAccess", true);
+        window.setTimeout(
+          $A.getCallback(function () {
+            var flow = component.find("insideSalesLeadFlow");
+            if (flow) {
+              flow.startFlow("SDO_Inside_Sales_Create_Lead");
             }
-            this.navigateToStandardNewLead();
-        });
-        $A.enqueueAction(action);
-    },
+          }),
+          0
+        );
+        return;
+      }
+      this.navigateToStandardNewLead();
+    });
+    $A.enqueueAction(action);
+  },
 
-    navigateToStandardNewLead: function () {
-        var navigate = $A.get('e.force:navigateToURL');
-        if (navigate) {
-            navigate.setParams({ url: '/lightning/o/Lead/new?nooverride=1' });
-            navigate.fire();
-        } else {
-            window.location.assign('/lightning/o/Lead/new?nooverride=1');
-        }
-    },
-
-    navigateToLeadList: function () {
-        var navigate = $A.get('e.force:navigateToURL');
-        if (navigate) {
-            navigate.setParams({ url: '/lightning/o/Lead/list?filterName=Recent' });
-            navigate.fire();
-        }
+  navigateToStandardNewLead: function () {
+    var navigate = $A.get("e.force:navigateToURL");
+    if (navigate) {
+      navigate.setParams({ url: "/lightning/o/Lead/new?nooverride=1" });
+      navigate.fire();
+    } else {
+      window.location.assign("/lightning/o/Lead/new?nooverride=1");
     }
+  },
+
+  navigateToLeadList: function () {
+    var navigate = $A.get("e.force:navigateToURL");
+    if (navigate) {
+      navigate.setParams({ url: "/lightning/o/Lead/list?filterName=Recent" });
+      navigate.fire();
+    } else {
+      window.location.assign("/lightning/o/Lead/list?filterName=Recent");
+    }
+  }
 });
